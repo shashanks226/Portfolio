9df47c4ccda765ffa911351b3120d3b678c34022
Shashank <shashankkumarrup5@gmail.com>
2026-04-09 02:01:35 +0530

    Project Image Problem Resolved

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,13 @@
+import freqflowImage from "../assets/images/freqflow_image.png";
+
+
+
+
 const projects = [
   {
     title: "Freq-Flow",
     desc: "FreqFlow is a real-time music visualizer that converts audio into stunning animated visuals using React and Three.js. Experience sound like never before.",
-    image: "./src/assets/images/freqflow_image.png",
+    image: freqflowImage,
     tech: ["Html","CSS", "Vanila Js", "React.js", "Three.js"],
     github: "https://github.com/shashanks226/FreqFlow",
     demo: "https://freq-flow.vercel.app/"
