6940336bbaffd8af05fd9f80209b5133bd1019bc
Shashank <shashankkumarrup5@gmail.com>
2026-05-08 13:54:01 +0530

    CV Update

--- src/utils/downloadCV.js
+++ src/utils/downloadCV.js
@@ -1,6 +1,6 @@
 export const downloadcv = () => {
   const link = document.createElement("a");
-  link.href = "\src\assets\Document\CV.pdf"; // file in public folder
+  link.href = "/CV.pdf"; // file in public folder
   link.download = "Shashank_Kumar_CV.pdf";
 
   document.body.appendChild(link);
